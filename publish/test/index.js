f421213adf7e5f32ad21dc8447aa3074f58d7c47
taurus-kuchain <taurus@kuchain.io>
2020-09-16 15:22:10 +0800

    return to my PC

--- publish/test/index.js
+++ publish/test/index.js
@@ -9,12 +9,17 @@ const address = function(name) {
 	return require('../deployed/local/deployment').targets[name].address;
 };
 
-const provider = new providers.JsonRpcProvider('http://192.168.1.183:8545');
-const privateKey = '0x5e6dff1276b3b05565cdc164ba734ff2a810c390b3196582ddbb126a331e6162';
+const provider = new providers.JsonRpcProvider('http://192.168.1.105:8545');
+const privateKey = '0xbced0c57065c2cf3b004c4d793949ef19fccd4697c39af6c9b5f6fa69150159a';
 const wallet = new Wallet(privateKey, provider);
 
 const systemContract = new Contract(address('SystemSettings'), abi('SystemSettings'), provider);
 const synthetixContract = new Contract(address('Synthetix'), abi('Synthetix'), provider);
+const synthetixStateContract = new Contract(
+	address('SynthetixState'),
+	abi('SynthetixState'),
+	provider
+);
 const ExchangeRatesContract = new Contract(
 	address('ExchangeRates'),
 	abi('ExchangeRates'),
@@ -24,6 +29,7 @@ const ExchangeRatesContract = new Contract(
 const synthetixWithSigner = synthetixContract.connect(wallet);
 const ExchangeRatesWithSigner = ExchangeRatesContract.connect(wallet);
 const systemWithSigner = systemContract.connect(wallet);
+const synthetixStateWithSigner = synthetixStateContract.connect(wallet);
 
 async function availableCurrencyKeys() {
 	const result = await synthetixWithSigner.availableCurrencyKeys();
@@ -51,13 +57,13 @@ async function rateStalePeriod() {
 }
 
 async function balanceOf() {
-	const result = await synthetixWithSigner.balanceOf('0x21E3ACce64A61Cf7d0a68C67A7F02C67fB48C797');
+	const result = await synthetixWithSigner.balanceOf('0x16c77205ffa3c66a2805469a1c800c164c2aaff5');
 	console.log('result -----', utils.formatEther(result));
 }
 
 async function transfer() {
 	const result = await synthetixWithSigner.transfer(
-		'0x6fD0E06Ce5bdfd2d1c0D06e822c03477FF0978f9',
+		'0x3b6d52282b4b602a8dd0096ed2e029bd1f16b262',
 		10000000
 	);
 	console.log('result -----', result);
@@ -106,6 +112,31 @@ async function setIssuanceRatio() {
 	console.log('result -----', result);
 }
 
+async function stateIssuanceRatio() {
+	const result = await synthetixStateWithSigner.issuanceRatio();
+	console.log('result -----', result);
+}
+
+async function collateralisationRatio() {
+	const result = await synthetixWithSigner.collateralisationRatio('0x16c77205ffa3c66a2805469a1c800c164c2aaff5');
+	console.log('result -----', result);
+}
+
+async function getEthBlance() {
+	provider.getBalance('0x16c77205ffa3c66a2805469a1c800c164c2aaff5').then((balance) => {
+
+		// 余额是 BigNumber (in wei); 格式化为 ether 字符串
+		let etherString = utils.formatEther(balance);
+
+		console.log('Balance: ' + etherString);
+	});
+}
+
+async function getNetwork() {
+	const result = await provider.getBlock();
+	console.log('result -----', result);
+}
+
 //availableCurrencyKeys()
 //totalIssuedSynths()
 //rateForCurrency
@@ -114,7 +145,11 @@ async function setIssuanceRatio() {
 //balanceOf
 //transfer
 //issueSynths()
-anyRateIsStale();
+//anyRateIsStale();
 //updateRates()
 //issuanceRatio()
 //setIssuanceRatio()
+//stateIssuanceRatio()
+//collateralisationRatio()
+//getEthBlance();
+getNetwork()
